dafb9362471c9cca7b8aa1f9abef44068bfed72a
Avinash <avisingh736@gmail.com>
2019-10-31 12:58:12 +0530

    Model and like counter added

--- app/src/main/java/com/example/compose_example/MainActivity.kt
+++ app/src/main/java/com/example/compose_example/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.compose_example
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.state
 import androidx.compose.unaryPlus
 import androidx.ui.core.*
 import androidx.ui.foundation.DrawImage
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.graphics.Color
 import androidx.ui.layout.*
+import androidx.ui.material.Button
 import androidx.ui.material.MaterialTheme
 import androidx.ui.res.imageResource
 import androidx.ui.text.ParagraphStyle
@@ -16,6 +18,7 @@ import androidx.ui.text.TextStyle
 import androidx.ui.text.font.FontFamily
 import androidx.ui.text.style.TextAlign
 import androidx.ui.tooling.preview.Preview
+import com.example.compose_example.model.Like
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,6 +42,7 @@ fun DefaultPreview() {
 @Composable
 fun MyCustomDesign() {
     DemoImage()
+    LikeCounter()
 }
 
 @Composable
@@ -47,7 +51,7 @@ fun DemoImage() {
         crossAxisSize = LayoutSize.Expand,
         modifier = Spacing(16.dp)
     ) {
-        Container(expanded = true, height = 150.dp,alignment = Alignment.BottomCenter) {
+        Container(expanded = true, height = 150.dp, alignment = Alignment.BottomCenter) {
             Clip(shape = RoundedCornerShape(size = 5.dp)) {
                 DrawImage(+imageResource(R.drawable.abc_1))
             }
@@ -73,7 +77,7 @@ fun DemoText(name: String) {
             )
         }
         Container(
-            expanded = true, padding = EdgeInsets(0.dp,0.dp,0.dp,5.dp)
+            expanded = true, padding = EdgeInsets(0.dp, 0.dp, 0.dp, 5.dp)
         ) {
             Text(
                 text = "This is an example of android jetpack compose toolkit, This is an example of android jetpack compose toolkit",
@@ -84,6 +88,33 @@ fun DemoText(name: String) {
                     fontFamily = FontFamily.Serif
                 )
             )
+        }
+    }
+}
+
+@Composable
+fun LikeCounter() {
+    val myLike = +state { Like(totalLikes = 0) }
+
+    Column(
+        mainAxisAlignment = MainAxisAlignment.End,
+        crossAxisAlignment = CrossAxisAlignment.End,
+        modifier = Spacing(16.dp)
+    ) {
+        Container {
+            Column(crossAxisAlignment = CrossAxisAlignment.Center) {
+                Text(text = "${myLike.value.totalLikes}")
+                Padding(padding = 5.dp) {
+                    Button(text = "like", onClick = { myLike.value.totalLikes += 1 })
+                }
+                Padding(padding = 5.dp) {
+                    Button(text = "dislike", onClick = {
+                        if (myLike.value.totalLikes > 0) {
+                            myLike.value.totalLikes -= 1
+                        }
+                    })
+                }
+            }
         }
     }
 
---
+++ app/src/main/java/com/example/compose_example/model/Like.kt
@@ -0,0 +1,6 @@
+package com.example.compose_example.model
+
+import androidx.compose.Model
+
+@Model
+data class Like(var totalLikes: Int)
